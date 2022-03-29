cdcaefe8bdb006892771e20ea3451d867cdc1fa7
Skronkie <ebrockway01@gmail.com>
2022-03-28 20:19:08 -0500

    added put and delete routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
+const { debugPort } = require("process");
 const { Tag, Product, ProductTag } = require("../../models");
+const seedTags = require("../../seeds/tag-seeds");
 
 // The `/api/tags` endpoint
 
@@ -39,14 +41,57 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name,
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No tag found with this id!" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: { id: req.params.id },
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "There is no tag with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
